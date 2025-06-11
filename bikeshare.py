29827113db09e7fe6bf31833f3a3a8f3fdc186a1
Mateus Barone <barone.mateus@gmail.com>
2025-06-11 18:07:50 +0200

    Code: Optimize CSV loading by using parse_dates for Start Time

--- bikeshare.py
+++ bikeshare.py
@@ -74,9 +74,10 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    df = pd.read_csv(CITY_DATA[city])
+    df = pd.read_csv(CITY_DATA[city], parse_dates=['Start Time'])
+    #df = pd.read_csv(CITY_DATA[city])
     # convert the Start Time column to datetime
-    df['Start Time'] = pd.to_datetime(df['Start Time'])
+    # df['Start Time'] = pd.to_datetime(df['Start Time'])
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.day_name()
